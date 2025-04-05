4b11e37657f52f8364cf50708dd6a3f81a204392
baicy <baicy0118@gmail.com>
2025-04-05 22:23:31 +0800

    feat: 项目初始化

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-footer
+    height="40"
+    color="secondary"
+    app
+  >
+    <v-btn
+      v-for="item in items"
+      :key="item.id"
+      :to="item.href"
+      color="secondary"
+    >
+      {{ item.title }}
+    </v-btn>
+  </v-footer>
+</template>
+
+<script setup lang="ts">
+  const items = [
+    {
+      id: 'log',
+      title: '事务',
+      href: '/log',
+    },
+    {
+      id: 'material',
+      title: '资源',
+      href: '/material',
+    },
+    {
+      id: 'story',
+      title: '故事',
+      href: '/story',
+    },
+  ]
+</script>
+
+<style scoped lang="sass">
+  .social-link :deep(.v-icon)
+    color: rgba(var(--v-theme-on-background), var(--v-disabled-opacity))
+    text-decoration: none
+    transition: .2s ease-in-out
+
+    &:hover
+      color: rgba(25, 118, 210, 1)
+</style>
---
+++ src/plugins/vuetify.ts
@@ -0,0 +1,35 @@
+/**
+ * plugins/vuetify.ts
+ *
+ * Framework documentation: https://vuetifyjs.com`
+ */
+
+// Styles
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
+import { md1 } from 'vuetify/blueprints'
+
+// Composables
+import { createVuetify } from 'vuetify'
+
+// https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
+export default createVuetify({
+  blueprint: md1,
+  theme: {
+    defaultTheme: 'rhodes',
+    themes: {
+      rhodes: {
+        dark: false,
+        colors: {
+          background: '#e8e8e8',
+          surface: '#fafafa',
+          primary: '#0098dc',
+          secondary: '#313131',
+          success: '#aacb62',
+          warning: '#ffd900',
+          error: '#9d4d64'
+        }
+      }
+    }
+  },
+})
