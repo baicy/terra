2a382a6dd924bfdcaa2243feb70692c5555cdee7
baicy <baicy0118@gmail.com>
2025-05-16 00:21:14 +0800

    fix: 放弃吧

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,7 +5,7 @@
  */
 
 // Composables
-import { createRouter, createWebHistory } from 'vue-router/auto'
+import { createRouter, createWebHashHistory } from 'vue-router/auto'
 import { setupLayouts } from 'virtual:generated-layouts'
 import { routes } from 'vue-router/auto-routes'
 
@@ -13,7 +13,7 @@ const autoRoutes = setupLayouts(routes)
 autoRoutes.find((v) => v.path == '/txt')!.path = '/txt/:chapters*'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: autoRoutes
 })
 
